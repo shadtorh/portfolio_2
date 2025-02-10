ad4c92bd40ef10116ccee074adbb86e331f2cda6
shadtorh <torhshad09@gmail.com>
2025-02-10 13:32:25 -0500

    my_portfolio

--- tailwind_portfolio/src/App.jsx
+++ tailwind_portfolio/src/App.jsx
@@ -1,11 +1,19 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import SectionTitle from "./components/SectionTitle";
+import Skills from "./components/Skills";
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
 			<Hero />
+			<SectionTitle />
+			<Skills />
+			<About />
+			<Projects />
 		</div>
 	);
 };
